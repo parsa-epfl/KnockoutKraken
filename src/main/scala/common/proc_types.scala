a42361b02f9d79f83c1b81e1eec1c59fff5f7d97
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2018-03-09 16:48:53 +0100

    General processor types

---
+++ src/main/scala/common/proc_types.scala
@@ -0,0 +1,31 @@
+package common
+
+import chisel3._
+import chisel3.util.BitPat
+
+/*
+ * Bit Patterns for Instructions
+ *
+ */
+object PROCESSOR_TYPES
+{
+  // Number of threads
+  def NUM_THREADS = 4
+  def W_NUM_THREAD = chisel3.util.log2Ceil(NUM_THREADS).W // 4 Threads
+  def TAG_T = UInt(W_NUM_THREAD)
+
+  // Data
+  def W_DATA = 64.W
+  def DATA_T = UInt(W_DATA)
+  val DATA_X = 0.U(W_DATA)
+
+  def W_INST = 32.W
+  def INST_T = UInt(W_INST)
+
+  // Regs
+  def W_REG = 5.W
+  def REG_T = UInt(W_REG)
+  val REG_X = 0.U(W_REG)
+
+}
+
