dee887f83165a35b93126ae48ab4a4bf5541d2f8
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2018-03-26 00:41:00 +0200

    Test folders same as package

--- src/test/scala/protoflex/DecodeTests.scala
+++ src/test/scala/protoflex/DecodeTests.scala
@@ -15,14 +15,15 @@ class DecodeTests(c: DecodeUnit) extends PeekPokeTester(c)
     Seq( "1",  "00",     "01010",      "01",   "0",    "01010",          "111000",           "01010",         "00000")
   val inst = BigInt(inst_bits reduce ( _ + _ ), 2) // ORR
 
-  poke(c.io.inst, inst)
-
+  poke(c.io.in_inst.bits, inst)
+  poke(c.io.in_inst.valid, true.B)
+  poke(c.io.in_tag, 0.U)
   step(1)
 
-  val out_data = Seq(c.io.dinst.rd, c.io.dinst.rs1, c.io.dinst.rs2, c.io.dinst.imm, c.io.dinst.shift)
+  val out_data = Seq(c.io.out_dinst.bits.rd, c.io.out_dinst.bits.rs1, c.io.out_dinst.bits.rs2, c.io.out_dinst.bits.imm, c.io.out_dinst.bits.shift)
   val exp_data = Seq( inst_bits(8),   inst_bits(7),   inst_bits(5),   inst_bits(6),     inst_bits(3)) map (x => BigInt(x, 2))
 
-  val out_ctrl = Seq(c.io.dinst.imm_valid,            c.io.dinst.aluOp, c.io.dinst.shift_v, c.io.dinst.valid)
+  val out_ctrl = Seq(c.io.out_dinst.bits.imm_en,            c.io.out_dinst.bits.aluOp, c.io.out_dinst.bits.shift_en, c.io.out_dinst.bits.inst_en)
   val exp_ctrl = Seq(                 "0", inst_bits(1) + inst_bits(4),                "1",              "1") map (x => BigInt(x, 2))
 
   out_data zip exp_data map ( m => expect(m._1, m._2))
@@ -33,8 +34,10 @@ class DecodeTester extends ChiselFlatSpec
 {
   behavior of "Decoder"
 
-  backends foreach {backend =>
-    it should s"decode an instruction" in {
+  private val backendNames = Array("firrtl")
+
+  for ( backend <- backendNames ) {
+    it should s"decode an instruction (with $backend)" in {
       Driver(() => new DecodeUnit, backend)((c) => new DecodeTests(c)) should be (true)
     }
   }
