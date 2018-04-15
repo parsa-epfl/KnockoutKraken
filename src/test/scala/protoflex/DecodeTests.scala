261e2c8ec3746418eece6e674c5b14057e1539ff
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2018-04-15 21:05:39 +0200

    Full tests for LogShiftReg decode, based on parsing assembly.

--- src/test/scala/protoflex/DecodeTests.scala
+++ src/test/scala/protoflex/DecodeTests.scala
@@ -5,29 +5,43 @@ import chisel3.iotesters
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
 import common.INSTRUCTIONS._
+import utils.AssemblyParser
+import utils.AssemblyInstruction
 
 
 class DecodeTests(c: DecodeUnit) extends PeekPokeTester(c)
 {
-  val inst_bits =
-    //   sf |  opc  |  0  1  0  1  0 | shift |  N |      Rm        |       imm6        |       Rn       |       Rd      |
-    //    1 |  0  0 |  0  1  0  1  0 | shift |  0 |      Rm        |       imm6        |       Rn       |       Rd      |
-    Seq( "1",  "00",     "01010",      "01",   "0",    "01010",          "111000",           "01010",         "00000")
-  val inst = BigInt(inst_bits reduce ( _ + _ ), 2) // ORR
+  val instrs = AssemblyParser.parse("decode.x")
 
-  poke(c.io.in_inst.bits, inst)
-  poke(c.io.in_inst.valid, true.B)
-  poke(c.io.in_tag, 0.U)
-  step(1)
+  val dinst_out = Seq(
+    c.io.out_dinst.bits.aluOp,
+    c.io.out_dinst.bits.rd,
+    c.io.out_dinst.bits.rs1,
+    c.io.out_dinst.bits.rs2,
+    c.io.out_dinst.bits.imm,
+    c.io.out_dinst.bits.shift,
+    c.io.out_dinst.bits.rd_en,
+    c.io.out_dinst.bits.rs1_en,
+    c.io.out_dinst.bits.rs2_en,
+    c.io.out_dinst.bits.imm_en,
+    c.io.out_dinst.bits.shift_en,
+    c.io.out_dinst.bits.inst_en
+  )
 
-  val out_data = Seq(c.io.out_dinst.bits.rd, c.io.out_dinst.bits.rs1, c.io.out_dinst.bits.rs2, c.io.out_dinst.bits.imm, c.io.out_dinst.bits.shift)
-  val exp_data = Seq( inst_bits(8),   inst_bits(7),   inst_bits(5),   inst_bits(6),     inst_bits(3)) map (x => BigInt(x, 2))
+  instrs map {
+    case inst =>
+      println(inst.line)
+      poke(c.io.in_inst.bits, inst.bitPat)
+      poke(c.io.in_inst.valid, 1)
+      poke(c.io.out_dinst.ready, 1)
+      poke(c.io.in_tag, 1)
 
-  val out_ctrl = Seq(c.io.out_dinst.bits.imm_en,            c.io.out_dinst.bits.aluOp, c.io.out_dinst.bits.shift_en, c.io.out_dinst.bits.inst_en)
-  val exp_ctrl = Seq(                 "0", inst_bits(1) + inst_bits(4),                "1",              "1") map (x => BigInt(x, 2))
+      step(1)
 
-  out_data zip exp_data map ( m => expect(m._1, m._2))
-  out_ctrl zip exp_ctrl map ( m => expect(m._1, m._2))
+      dinst_out zip inst.io map { case (out, in) => expect(out, in)}
+
+      expect(c.io.out_dinst.bits.tag, 1)
+  }
 }
 
 class DecodeTester extends ChiselFlatSpec
