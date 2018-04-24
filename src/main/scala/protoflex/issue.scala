b152d482b5445ec179721626f2fca9fb9051dc75
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2018-04-24 15:05:21 +0200

    Issue unit queueing, arbiter tested and working

--- src/main/scala/protoflex/issue.scala
+++ src/main/scala/protoflex/issue.scala
@@ -7,17 +7,18 @@ import chisel3.util.log2Ceil
 import chisel3.util.EnqIO
 import chisel3.util.DeqIO
 import chisel3.util.PriorityEncoder
+import chisel3.util.Reverse
+import chisel3.util.PriorityMux
+
 
 import common.PROCESSOR_TYPES._
 
 class IssueUnitIO extends Bundle {
   // Decode - Issue
-  val enq = EnqIO(new DInst)
-  val tag = Input(TAG_T)
+  val enq = DeqIO(new DInst)
 
   // Issue - Exec
-  val deq = DeqIO(new DInst)
-
+  val deq = EnqIO(new DInst)
 
   //`Exec - Issue
   val exe_rd = Input(REG_T)
@@ -45,13 +46,13 @@ class IssueUnit extends Module
 
   /** Issue stage pipeline register
     * reg_pipe   Contains the next decoded instruction to issue per thread.
-    *            The Round Robine Arbiter selects which thread issues during the cycle.
+    *            The Round Robine Arbiter selects which threassangead issues during the cycle.
     * reg_pipe_v The valid bit indicates whenever the register contains a valid decoded instruction.
     * sig_pipe_r This signal indicates whenever the register is ready to recieve the next instruction.
     */
-  val reg_pipe   = RegInit(VecInit(Seq.fill(NUM_THREADS)(new DInst)))
-  val reg_pipe_v = RegInit(VecInit(0.U(NUM_THREADS.W).toBools))
-  val sig_pipe_r = WireInit(VecInit(UInt(NUM_THREADS.W).toBools))
+  val reg_pipe   = RegInit(VecInit(Seq.fill(NUM_THREADS)(Wire(new DInst).empty())))
+  val reg_pipe_v = RegInit(VecInit(TAG_VEC_X.toBools))
+  val sig_pipe_r = WireInit(VecInit(TAG_VEC_X.toBools))
 
   /** Issue stage buffer
     * FIFO_i To buffer instructions we have a FIFO queue with flow and pipe modes enabled per thread.
@@ -67,19 +68,17 @@ class IssueUnit extends Module
 
   /** Issue stage arbiter
     * arbiter        Round Robin Arbiter
-    * reg_issue_idx  Contains the index of the last issued thread
     * sig_pipe_i     This signal indicates which threads are ready to be issued
     * sig_next_idx   This signal indicates the next instruction to issue
     */
-  val arbiter       = new RRArbiter(NUM_THREADS)
-  val reg_issue_idx = Reg(0.U(TAG_T))
-  val sig_pipe_i    = Wire(UInt(NUM_THREADS.W))
-  val sig_next_idx  = Wire(TAG_T)
+  val arbiter       = Module(new RRArbiter(NUM_THREADS))
+  val sig_pipe_i    = VecInit(TAG_VEC_X.toBools)
+  val sig_next_idx  = WireInit(TAG_X)
 
   /** Managing backpressure
     */
-  val exe_stall = WireInit(Bool())
-  val mem_stall = WireInit(Bool())
+  val exe_stall = WireInit(false.B)
+  val mem_stall = WireInit(false.B)
 
   // Enqueuing interface Decode -> FIFO
   // Enqueues the Decoded instrcution with matching tag
@@ -87,12 +86,12 @@ class IssueUnit extends Module
     f.enq.valid := false.B
     f.enq.bits := io.enq.bits
   }
-  io.enq.ready := fifo_vec(io.tag).enq.ready
-  fifo_vec(io.tag).enq.valid := io.enq.valid
+  io.enq.ready := fifo_vec(io.enq.bits.tag).enq.ready
+  fifo_vec(io.enq.bits.tag).enq.valid := io.enq.valid
 
   // Dequeing buffers interface : FIFO -> Reg
   // Registers who are invalid are ready to recieve new value
-  sig_pipe_r zip reg_pipe_v map { case (reg_r, reg_v) => reg_r := ~reg_v}
+  sig_pipe_r zip reg_pipe_v map { case (sig_r, reg_v) => sig_r := ~reg_v}
   fifo_vec zip (reg_pipe, reg_pipe_v, sig_pipe_r).zipped.toSeq map {
     case (fifo, (reg, reg_v, sig_r)) =>
       fifo.deq.ready := sig_r
@@ -102,7 +101,8 @@ class IssueUnit extends Module
 
   // Choose next thread to Issue (Reg status + hazards -> idx)
   // Thread is ready to be issued if it has a valid decoded instruction in register and has no hazard detected.
-  sig_pipe_i.toBools zip reg_pipe_v map { case (sig_rdy,reg_v) => sig_rdy := reg_v}
+  sig_pipe_i zip reg_pipe_v map { case (sig_rdy,reg_v) => sig_rdy := reg_v}
+
   exe_stall := (io.exe_rd_v && ((reg_pipe(io.exe_tag).rs1 === io.exe_rd) || (reg_pipe(io.exe_tag).rs2 === io.exe_rd)))
   mem_stall := (io.mem_rd_v && ((reg_pipe(io.mem_tag).rs1 === io.mem_rd) || (reg_pipe(io.mem_tag).rs2 === io.mem_rd)))
   when(io.exe_tag === io.mem_tag) {
@@ -112,41 +112,57 @@ class IssueUnit extends Module
     sig_pipe_i(io.mem_tag) := reg_pipe_v(io.mem_tag) && !mem_stall
   }
 
+  // Issue -> Exec
   // Get idx to issue
-  arbiter.io.ready := sig_pipe_i
-  arbiter.io.last  := reg_issue_idx
-  sig_next_idx     := arbiter.io.next
+  arbiter.io.ready := sig_pipe_i.asUInt
+  sig_next_idx     := arbiter.io.next.bits
 
   io.deq.bits  := reg_pipe(sig_next_idx)
-  io.deq.valid := false.B
-  // If at least one of the threads is ready to be issued
-  when(sig_pipe_i.orR() && io.deq.ready) {
-    // Set issued thread register ready to recieve next instruction
-    sig_pipe_r(sig_next_idx) := true.B
-    // Update for next round
-    reg_issue_idx := sig_next_idx
-    // Issues the thread : Reg -> Exec
-    io.deq.valid := true.B
+  io.deq.valid := arbiter.io.next.valid
+  arbiter.io.next.ready := io.deq.ready
+
+  // If next stage ready, and one of the threads is ready to be issued
+  // Set issued thread register ready to recieve next instruction
+  when(arbiter.io.next.valid && io.deq.ready) {
+    fifo_vec(sig_next_idx).deq.ready := true.B
+    reg_pipe_v(sig_next_idx) := fifo_vec(sig_next_idx).deq.valid
+    reg_pipe(sig_next_idx) := fifo_vec(sig_next_idx).deq.bits
   }
 }
 
 class RRArbiter(arbN: Int)
     extends Module {
 
-  val io = new Bundle {
-    val ready = Input(UInt(arbN.W))            // Ready threads
-    val last  = Input(UInt(log2Ceil(arbN).W))  // Last thread issued
-    val next  = Output(UInt(log2Ceil(arbN).W)) // Next thread to issue
+  val io = IO(new Bundle {
+           // Ready threads
+           val ready = Input(UInt(arbN.W))
+           // Next thread to issue
+           val next  = EnqIO(UInt(log2Ceil(arbN).W))
+         })
+
+  def rotateRight[T <: Data](norm: UInt, rot : UInt) : UInt = {
+    val right = norm >> rot
+    val left  = norm << ~rot + 1.U;
+    val res = left | right
+    res(arbN - 1, 0)
   }
 
-  def rotateLeft[T <: Data](norm: Vec[T], rot: UInt): Vec[T] = {
-    val n = norm.size
-    VecInit.tabulate(n) { i =>
-      Mux(rot < (n - i).U, norm(i.U + rot), norm(rot - (n - i).U))
-    }
-  }
+  /*
+   * valid      At least one thread is ready to be issued
+   * curr       Thread with highest priority during round
+   * ready_ofst Ready vector shifted such as lsb is the
+   *            offset from curr thread to the thread with
+   *            highest priority
+   * ofst       Offset from curr thread to thread to be issued
+   * next       Thread being issued on this cycle if next stage ready
+   */
+  val valid = io.ready.orR
+  val curr = RegInit(0.U(log2Ceil(arbN).W))  // Last thread issued
+  val ready_ofst = rotateRight(io.ready, curr)
+  val ofst = PriorityEncoder(ready_ofst)
+  val next = curr + ofst
 
-  // Shift such as the lsb is the thread with highest priority
-  val ready_next = rotateLeft(VecInit(io.ready), io.last + 1.U)
-  io.next := PriorityEncoder(ready_next.asUInt)
+  io.next.bits := next
+  io.next.valid := valid
+  when(io.next.ready && valid) { curr := next + 1.U}
 }
---
+++ src/test/scala/protoflex/IssueTests.scala
@@ -0,0 +1,230 @@
+package protoflex
+
+import chisel3._
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+import utils.AssemblyParser
+import utils.AssemblyInstruction
+import common.DECODE_INTEGER_LITERALS._
+
+class IssueTestsReadyValid(c: IssueUnit) extends PeekPokeTester(c)
+{
+  val dinst_in = Seq(
+    c.io.enq.bits.aluOp,
+    c.io.enq.bits.rd,
+    c.io.enq.bits.rs1,
+    c.io.enq.bits.rs2,
+    c.io.enq.bits.imm,
+    c.io.enq.bits.shift,
+    c.io.enq.bits.rd_en,
+    c.io.enq.bits.rs1_en,
+    c.io.enq.bits.rs2_en,
+    c.io.enq.bits.imm_en,
+    c.io.enq.bits.shift_en,
+    c.io.enq.bits.inst_en
+  )
+
+  val dinst_out = Seq(
+    c.io.deq.bits.aluOp,
+    c.io.deq.bits.rd,
+    c.io.deq.bits.rs1,
+    c.io.deq.bits.rs2,
+    c.io.deq.bits.imm,
+    c.io.deq.bits.shift,
+    c.io.deq.bits.rd_en,
+    c.io.deq.bits.rs1_en,
+    c.io.deq.bits.rs2_en,
+    c.io.deq.bits.imm_en,
+    c.io.deq.bits.shift_en,
+    c.io.deq.bits.inst_en
+  )
+
+  val exe_in = Seq(
+    c.io.exe_rd,
+    c.io.exe_tag,
+    c.io.exe_rd_v
+  )
+  val mem_in = Seq(
+    c.io.mem_rd,
+    c.io.mem_tag,
+    c.io.mem_rd_v
+  )
+
+  // creates n instructions with target and source registers index of i
+  val n = 3
+  val insts = (0 to n) map { case i => AssemblyInstruction(i: Int, i: Int, i: Int) }
+
+  // Fill the queue, check whenever ready signal goes low
+  poke(c.io.enq.valid, 0)
+  poke(c.io.deq.ready, 0)
+  exe_in map { s => poke(s, 0) }
+  mem_in map { s => poke(s, 0) }
+
+  println()
+  println("Enqueing")
+  for (i <- 0 until 3) {
+    println("Cycle" + i)
+    poke(c.io.enq.valid, 1)
+    poke(c.io.enq.bits.tag, 2)
+    dinst_in zip insts(i).io map { case (io, in) => poke(io, in)}
+
+    if(i == 3) expect(c.io.enq.ready, 0)
+    else       expect(c.io.enq.ready, 1)
+
+    if(i == 0) {
+      expect(c.io.deq.valid, 0)
+    } else {
+      expect(c.io.deq.valid, 1)
+      expect(c.io.deq.bits.tag, 2)
+      expect(c.io.deq.bits.rd, 0)
+    }
+    step(1)
+  }
+
+  // Dequeing
+  println()
+  println("Dequeing")
+  // Confirm queue is full, and others empty
+  expect(c.io.enq.ready, 0)
+  poke(c.io.enq.bits.tag, 0)
+  expect(c.io.enq.ready, 1)
+  poke(c.io.enq.bits.tag, 2)
+  expect(c.io.enq.ready, 0)
+  for (i <- 0 until 3) {
+    println("Cycle" + i)
+    poke(c.io.deq.ready, 1)
+    expect(c.io.enq.ready, 1)
+
+    if(i == 3) {
+      expect(c.io.deq.valid, 0)
+    } else {
+      expect(c.io.deq.valid, 1)
+      expect(c.io.deq.bits.tag, 2)
+      expect(c.io.deq.bits.rd, i)
+    }
+
+    step(1)
+  }
+}
+
+class IssueTestsPriority(c: IssueUnit) extends PeekPokeTester(c)
+{
+  val dinst_in = Seq(
+    c.io.enq.bits.aluOp,
+    c.io.enq.bits.rd,
+    c.io.enq.bits.rs1,
+    c.io.enq.bits.rs2,
+    c.io.enq.bits.imm,
+    c.io.enq.bits.shift,
+    c.io.enq.bits.rd_en,
+    c.io.enq.bits.rs1_en,
+    c.io.enq.bits.rs2_en,
+    c.io.enq.bits.imm_en,
+    c.io.enq.bits.shift_en,
+    c.io.enq.bits.inst_en
+  )
+  val dinst_out = Seq(
+    c.io.deq.bits.aluOp,
+    c.io.deq.bits.rd,
+    c.io.deq.bits.rs1,
+    c.io.deq.bits.rs2,
+    c.io.deq.bits.imm,
+    c.io.deq.bits.shift,
+    c.io.deq.bits.rd_en,
+    c.io.deq.bits.rs1_en,
+    c.io.deq.bits.rs2_en,
+    c.io.deq.bits.imm_en,
+    c.io.deq.bits.shift_en,
+    c.io.deq.bits.inst_en
+  )
+  val exe_in = Seq(
+    c.io.exe_rd,
+    c.io.exe_tag,
+    c.io.exe_rd_v
+  )
+  val mem_in = Seq(
+    c.io.mem_rd,
+    c.io.mem_tag,
+    c.io.mem_rd_v
+  )
+
+  // creates n instructions with target and source registers index of i
+  val n = 16
+  val insts = (0 to n) map { case i => AssemblyInstruction(i: Int, i: Int, i: Int) }
+
+  def cycle(cycle : Int, enq_val : Boolean, inst : Int, tag : Int, enq_rdy : Boolean, deq_valid : Boolean, deq_rdy : Boolean, deq_tag : Int, deq_rd : Int) : Unit = {
+    println("Cycle" +  cycle)
+    poke(c.io.enq.valid, enq_val)
+    poke(c.io.enq.bits.tag, tag)
+    poke(c.io.deq.ready, deq_rdy)
+    dinst_in zip insts(inst).io map { case (io, in) => poke(io,in) }
+    expect(c.io.enq.ready, enq_rdy)
+    expect(c.io.deq.valid, deq_valid)
+    if( deq_rdy ) {
+      expect(c.io.deq.bits.tag, deq_tag)
+      expect(c.io.deq.bits.rd, deq_rd)
+    }
+    step(1)
+  }
+
+
+  // Fill the queue, check whenever ready signal goes low
+  poke(c.io.enq.valid, 0)
+  poke(c.io.deq.ready, 0)
+  poke(c.io.enq.bits.tag, 0)
+  exe_in map { s => poke(s, 0) }
+  mem_in map { s => poke(s, 0) }
+  println()
+  step(1)
+  expect(c.io.enq.ready, 1)
+  expect(c.io.deq.valid, 0)
+  // t:thread, 1,0 queue, R: pipe_reg, ~:last_idx    t  1  0  R ~       +:incoming  -:outgoing ->:deq.ready
+  //                                                  3            2           1           0
+  cycle( 0,  true, 0, 3,  true, false, false, 0, 0) //#|  |  |+0| |#|  |  |  | #|  |  |  | #|  |  |  |~
+  cycle( 1,  true, 1, 2,  true,  true, false, 0, 0) //#|  |  | 0| |#|  |  |+1| #|  |  |  | #|  |  |  |~
+  cycle( 2,  true, 2, 2,  true,  true, false, 0, 0) //#|  |  | 0| |#|  |+2| 1| #|  |  |  | #|  |  |  |~
+  cycle( 3,  true, 3, 2,  true,  true, false, 0, 0) //#|  |  | 0| |#|+3| 2| 1| #|  |  |  | #|  |  |  |~
+  cycle( 4,  true,15, 2, false,  true, false, 0, 0) //#|  |  | 0| |#| 3| 2| 1| #|  |  |  | #|  |  |  |~
+  cycle( 5,  true, 4, 1,  true,  true,  true, 2, 1) //#|  |  | 0| |#| 3| 2|-1|~#|  |  |+4| #|  |  |  | ->
+  cycle( 6,  true, 5, 1,  true,  true,  true, 3, 0) //#|  |  |-0|~|#|  | 3| 2| #|  |+5| 4| #|  |  |  | ->
+  cycle( 7,  true, 6, 1,  true,  true,  true, 1, 4) //#|  |  |  | |#|  | 3| 2| #|+6| 5|-4|~#|  |  |  | ->
+  cycle( 8, false, 0, 0,  true,  true,  true, 2, 2) //#|  |  |  | |#|  | 3|-2|~#|  | 6| 5| #|  |  |  | ->
+  cycle( 9, false, 0, 0,  true,  true,  true, 1, 5) //#|  |  |  | |#|  |  | 3| #|  | 6|-5|~#|  |  |  | ->
+  cycle(10, false, 0, 0,  true,  true,  true, 2, 3) //#|  |  |  | |#|  |  |-3|~#|  |  | 6| #|  |  |  | ->
+  cycle(11, false, 0, 0,  true,  true,  true, 1, 6) //#|  |  |  | |#|  |  |  | #|  |  |-6|~#|  |  |  | ->
+  cycle(12, false, 0, 0,  true, false,  true, 0, 0) //#|  |  |  | |#|  |  |  | #|  |  |  | #|  |  |  | ->
+}
+
+class IssueTester extends ChiselFlatSpec
+{
+  behavior of "Issuer"
+
+  private val backendNames = Array("firrtl")
+ 
+  for ( backend <- backendNames ) {
+    "IssueTestsReadyValid" should s"test IssueUnit Ready Valid signals for queues (with $backend)" in {
+      Driver(() => new IssueUnit, backend)((c) => new IssueTestsReadyValid(c)) should be (true)
+    }
+  }
+
+  for ( backend <- backendNames ) {
+    "IssueTestsPriority" should s"test IssueUnit arbiter (with $backend)" in {
+      Driver(() => new IssueUnit, backend)((c) => new IssueTestsPriority(c)) should be (true)
+    }
+  }
+}
+
+/**
+  * This provides a way to ruin the firrtl-interpreter REPL (or shell)
+  * on the lowered firrtl generated by your circuit. You will be placed
+  * in an interactive shell. This can be very helpful as a debugging
+  * technique. Type help to see a list of commands.
+  */
+object IssueRepl extends App {
+  iotesters.Driver.executeFirrtlRepl(args, () => new RRArbiter(4))
+}
+
+object IssueMain extends App {
+  chisel3.Driver.execute(args, () => new IssueUnit)
+}
