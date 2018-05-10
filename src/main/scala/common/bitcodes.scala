e1de595fa688a6cbf56354d7b1a922acb770ba42
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2018-05-10 20:37:08 +0200

    Fixed wrong bitcode for B.Cond

--- src/main/scala/common/bitcodes.scala
+++ src/main/scala/common/bitcodes.scala
@@ -60,8 +60,8 @@
 
 /* Compare & branch (immediate)            Bits[31:24] */
 /* 31 30 29 28 27 26 25 | 24 | 23 22 21 20 19 18 17 16 15 14 13 12 11 10 09 08 07 06 05 | 04 | 03 02 01 00 | Instruction Page | Variant   */
-/*  0  0  0  1  0  1  0 | o1 |                             imm19                        | o0 |    cond     |                  |           */
-/*  0  0  0  1  0  1  0 |  0 |                             imm19                        |  0 |    cond     | B.cond           |    -      */
+/*  0  1  0  1  0  1  0 | o1 |                             imm19                        | o0 |    cond     |                  |           */
+/*  0  1  0  1  0  1  0 |  0 |                             imm19                        |  0 |    cond     | B.cond           |    -      */
 
 /* Exception generation                  Bits[31:24] */
 /* 31 30 29 28 27 26 25 24 | 23 22 21 | 20 19 18 17 16 15 14 13 12 11 10 09 08 07 06 05 | 04 03 02 | 01 00 | Instruction Page | Variant */
