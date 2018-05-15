59f556a966a55dd7426dbbbcf46935ce70284a0f
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2018-05-15 21:19:17 +0200

    Regex fix, Cond map fix, spacing, B instruction parsing

--- src/main/scala/util/parser.scala
+++ src/main/scala/util/parser.scala
@@ -58,14 +58,12 @@ object AssemblyInstruction
     "ORR" -> OP_ORR,
     "ORN" -> OP_ORN,
     "EOR" -> OP_EOR,
-    "EON" -> OP_EON,
-    "ADD" -> OP_ADD,
-    "SUB" -> OP_SUB
+    "EON" -> OP_EON
   )
 
-  def BCond = Map {
+  def Branch = Map (
     "B" -> OP_BCOND
-  }
+  )
 
   def ShiftTypes = Map(
     "LSL" -> LSL,
@@ -74,26 +72,26 @@ object AssemblyInstruction
     "ROR" -> ROR
   )
 
-  def CondTypes = Map {
-    "EQ" -> EQ
-    "NE" -> NE
-    "CS" -> CS
-    "HS" -> HS
-    "CC" -> CC
-    "LO" -> LO
-    "MI" -> MI
-    "PL" -> PL
-    "VS" -> VS
-    "VC" -> VC
-    "HI" -> HI
-    "LS" -> LS
-    "GE" -> GE
-    "LT" -> LT
-    "GT" -> GT
-    "LE" -> LE
-    "AL" -> AL
+  def CondTypes = Map (
+    "EQ" -> EQ,
+    "NE" -> NE,
+    "CS" -> CS,
+    "HS" -> HS,
+    "CC" -> CC,
+    "LO" -> LO,
+    "MI" -> MI,
+    "PL" -> PL,
+    "VS" -> VS,
+    "VC" -> VC,
+    "HI" -> HI,
+    "LS" -> LS,
+    "GE" -> GE,
+    "LT" -> LT,
+    "GT" -> GT,
+    "LE" -> LE,
+    "AL" -> AL,
     "NV" -> NV
-  }
+  )
 
   /**
     * Creates a simple instruction with registers enabled
@@ -134,7 +132,7 @@ object AssemblyInstruction
     var shift = SHIFT_X
     var cond  = COND_X
 
-   (i_rd, i_rs1, i_rs2, i_imm, i_shift, i_cond) match {
+    (i_rd, i_rs1, i_rs2, i_imm, i_shift, i_cond) match {
       // Logical (shifted register)
       case (Some(d), Some(s1), Some(s2), _, _, _) if LogSR.get(i_op.toUpperCase).isDefined => {
         itype = I_LogSR
@@ -155,17 +153,21 @@ object AssemblyInstruction
       }
 
       // Compare & branch (immediate)
-      case (_, _, _, Some(i), _, Some(c)) if BCond.get(i_op.toUpperCase).isDefined => {
+      case (_, _, _, Some(i), _, Some(c)) if Branch.get(i_op.toUpperCase).isDefined => {
         itype = I_BCImm
 
-        op = BCond.getOrElse(i_op.toUpperCase, OP_ALU_X)
-
+        op = OP_BCOND
         rd  = i_rd match { case Some(d) => d; case _ => REG_X }
-        rs1 = i_rs1 match { case Some(s1) => s1; case _ => REG_X}
-        rs2 = i_rs2 match { case Some(s2) => s2; case _ => REG_X}
         imm = i
-
         cond = CondTypes.getOrElse(c.toUpperCase, COND_X)
+      }
+
+      // Branch ( immediate )
+      case (_, _, _, Some(i), _, _) if Branch.get(i_op.toUpperCase).isDefined => {
+        itype = I_BImm
+        op = OP_B
+        rd  = i_rd match { case Some(d) => d; case _ => REG_X }
+        imm = i
       }
 
       case _ =>
@@ -236,28 +238,26 @@ object AssemblyParser
   /*
 
    Regex 2 (branches) :
-   [a,b,c,d,e0-9]{1,}[?:]\s*([0-9a-z]{8,8})\s*([a-z]*)\s*([.]|\s*)(le|eq|ne|cs|cc|mi|pl|vs|vc|hi|ls|ge|lt|gt|al|nv|)(w|x([0-9a-z]*),\s*|\s*|)([0-9a-z]*)\s*<.*>
+   [a,b,c,d,e0-9]{1,}[?:]\s*([0-9a-z]{8,8})\s*([a-z]*)\s*([.]|\s*)(le|eq|ne|cs|cc|mi|pl|vs|vc|hi|ls|ge|lt|gt|al|nv|)\s*([a-z0-9]*).*<.*>.*
    Group information :
-   group       1  2      6    7     8
-   1c:	90000000 	adrp	x0, 258 <main+0x258>
-
-   group       1  2  7     8
+   group       1  2  5
    10:	14000013 	b	5c <main+0x5c>
 
-   group       1  2  4   7     8
+   group       1  2  4   5
    64:	54fffd8d 	b.le	14 <main+0x14>
 
    */
 
-  val regex_str_2 = "[a,b,c,d,e0-9]{1,}[?:]\\s*([0-9a-z]{8,8})\\s*([a-z]*)\\s*([.]|\\s*)(le|eq|ne|cs|cc|mi|pl|vs|vc|hi|ls|ge|lt|gt|al|nv|)(w|x([0-9a-z]*),\\s*|\\s*|)([0-9a-z]*)\\s*<.*>"
- 
+  val regex_str_2 =
+    "[a,b,c,d,e0-9]{1,}[?:]\\s*([0-9a-z]{8,8})\\s*([a-z]*)\\s*([.]|\\s*)(le|eq|ne|cs|cc|mi|pl|vs|vc|hi|ls|ge|lt|gt|al|nv|)\\s*([a-z0-9]*).*<.*>.*"
+
   val regex_2     = new Regex(regex_str_2)
   val grouVals_2 = Map(
     "bitPat"  -> Seq(1),
     "op"      -> Seq(2),
-    "rd"      -> Seq(6),
+    "rd"      -> Seq(),
     "sp"      -> Seq(), // Or PC
-    "imm"     -> Seq(7),
+    "imm"     -> Seq(5),
     "rs1"     -> Seq(),
     "rs2"     -> Seq(),
     "cond"    -> Seq(4),
@@ -274,36 +274,30 @@ object AssemblyParser
     for ( line <- file.getLines ) {
       regex_1.findAllMatchIn(line) foreach {
         matches =>
-          val op     = grouVals_1("op")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
-          val rd     = grouVals_1("rd")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val rs1    = grouVals_1("rs1")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val rs2    = grouVals_1("rs2")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val imm    = grouVals_1("imm")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val cond   = grouVals_1("cond")   map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s); case _ => None}
-          val shift  = grouVals_1("shift")  map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s); case _ => None}
-          val bitPat = grouVals_1("bitPat") map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
+        val op     = grouVals_1("op")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
+        val rd     = grouVals_1("rd")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
+        val rs1    = grouVals_1("rs1")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
+        val rs2    = grouVals_1("rs2")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
+        val imm    = grouVals_1("imm")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
+        val cond   = grouVals_1("cond")   map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s); case _ => None}
+        val shift  = grouVals_1("shift")  map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s); case _ => None}
+        val bitPat = grouVals_1("bitPat") map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
 
-          insts = insts :+ AssemblyInstruction(op, rd, rs1, rs2, imm, shift, cond, bitPat, line)
+        insts = insts :+ AssemblyInstruction(op, rd, rs1, rs2, imm, shift, cond, bitPat, line)
       }
-        //          case e : Exception => throw new Exception("Regex multi register op : matched wrong, non integer where expected integer (pos:" + file.pos + ") in line :\n\t" + line)
-      /*
+      //          case e : Exception => throw new Exception("Regex multi register op : matched wrong, non integer where expected integer (pos:" + file.pos + ") in line :\n\t" + line)
       regex_2.findAllMatchIn(line) foreach {
         matches =>
-        try {
-          val op     = grouVals_2("op")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
-          val rd     = grouVals_2("rd")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val rs1    = grouVals_2("rs1")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val rs2    = grouVals_2("rs2")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val imm    = grouVals_2("imm")    map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
-          val cond   = grouVals_1("cond")   map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s); case _ => None}
-          val shift  = grouVals_2("shift")  map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s); case _ => None}
-          val bitPat = grouVals_2("bitPat") map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
-          insts = insts :+ AssemblyInstruction(op.toUpperCase, rd, rs1, rs2, imm, shift, cond, bitPat, line)
-        } catch {
-          case e : Exception => throw new Exception("Regex branches : matched wrong, non integer where expected integer (pos:" + file.pos + ") in line :\n\t" + line)
-        }
+        val op = grouVals_2("op")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
+        val rd = grouVals_2("rd")     map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s.toInt); case _ => None}
+        val rs1 = None
+        val rs2 = None
+        val imm  = grouVals_2("imm") map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(BigInt(s,16).toInt); case _ => None}
+        val cond = grouVals_2("cond") map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => Some(s); case _ => None}
+        val shift  = None
+        val bitPat = grouVals_2("bitPat") map ( n => matches.group(n)) find( s => s != null && s != "") match { case Some(s) => s; case _ => ""}
+        insts = insts :+ AssemblyInstruction(op.toUpperCase, rd, rs1, rs2, imm, shift, cond, bitPat, line)
       }
-      */
     }
     insts
   }
