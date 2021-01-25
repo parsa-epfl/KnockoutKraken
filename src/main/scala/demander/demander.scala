b3e3c8dfc9d7c0c2ba5bc827e8f23528ea0817af
Shanqing Lin <xusine@outlook.com>
2021-01-25 20:46:07 +0100

    Almost done in the hardware part! (Not debug yet)

--- src/main/scala/demander/demander.scala
+++ src/main/scala/demander/demander.scala
@@ -67,17 +67,15 @@ class PageDemander(
   u_r_mmq.request_i <> u_bus.slave_requests_o(1)
   u_r_mmq.reply_o <> u_bus.slave_replies_i(1)
 
-  // TODO:  u_r_mmq.queue_i
-
   // The truth table (with parameter)
   // lookupThreadTable
   val u_truth_table = Module(new peripheral.ThreadTable())
   u_truth_table.request_i <> u_bus.slave_requests_o(2)
   u_truth_table.reply_o <> u_bus.slave_replies_i(2)
 
-  // TODO: u_truth_table.S_AXI
+  u_truth_table.S_AXI // from QEMU (lite) (R/W)
 
-  // TODO: the page table set manager
+  // TThe page table set manager
   // loadPTSet
   // lookupPT
   // getLRU
@@ -86,7 +84,7 @@ class PageDemander(
   val u_ptset = Module(new peripheral.PTSetCache())
   u_ptset.reply_o <> u_bus.slave_replies_i(3)
   u_ptset.request_i <> u_bus.slave_requests_o(3)
-  // u_ptset.M_AXI
+  u_ptset.M_AXI // to DRAM (R/W)
 
   // The TLB wrapper
   // responseToTLB
@@ -101,30 +99,97 @@ class PageDemander(
 
   u_tlb_wrapper.reply_o <> u_bus.slave_replies_i(4)
   u_tlb_wrapper.request_i <> u_bus.slave_requests_o(4)
-  // TODO: u_tlb_wrapper.tlb_backend_reply_o
-  // TODO: u_tlb_wrapper.tlb_flush_request_o
-  // TODO: u_tlb_wrapper.tlb_frontend_reply_i
+
+  u_tlb_wrapper.tlb_backend_reply_o
+  u_tlb_wrapper.tlb_flush_request_o
+  u_tlb_wrapper.tlb_frontend_reply_i
 
 
-  // TODO: the free list
+  // Free PA list
   // getFreePPN
   // recyclePPN
   val u_freelist = Module(new peripheral.FreeListWrapper(
     1 << 24
   ))
-  // u_freelist.M_AXI
-  // u_freelist.empty_vo
-  // u_freelist.full_vo
+  u_freelist.M_AXI // to DRAM (R/W)
+  u_freelist.empty_vo
+  u_freelist.full_vo
   u_freelist.reply_o <> u_bus.slave_replies_i(5)
   u_freelist.request_i <> u_bus.slave_requests_o(5)
 
-
-  // TODO: MMWriteQueue to QEMU.
   // sendMissRequestToQEMU
-  val u_w_mmq = Module(new peripheral.MMWriteQueue(new software_bundle.QEMUMessage))
-  u_w_mmq.request_i <> u_bus.slave_requests_o(5)
-  u_w_mmq.reply_o <> u_bus.slave_replies_i(5)
-  // u_w_mmq.queue_o
+  val u_miss_to_qemu = Module(new peripheral.MMWriteQueue(new software_bundle.PageFaultRequest()))
+
+  u_miss_to_qemu.request_i <> u_bus.slave_requests_o(6)
+  u_miss_to_qemu.reply_o <> u_bus.slave_replies_i(6)
+
+  // Page deletor
+  // movePageToQEMU
+  val u_page_deletor = Module(new peripheral.PageDeletor(
+    param.toCacheParameter()
+  ))
+
+  u_bus.slave_requests_o(7) <> u_page_deletor.request_i
+  u_bus.slave_replies_i(7) <> u_page_deletor.reply_o
+
+  u_page_deletor.M_AXI // to DRAM (R)
+  u_page_deletor.dcache_flush_request_o
+  u_page_deletor.dcache_wb_queue_empty_i
+
+  u_page_deletor.icache_flush_request_o
+  u_page_deletor.icache_wb_queue_empty_i
+  
+
+  // Page Inserter
+  // insertPageFromQEMU
+  val u_page_inserter = Module(new peripheral.PageInserter)
+  u_page_inserter.M_AXI // to DRAM (W)
+  
+  u_page_inserter.reply_o <> u_bus.slave_replies_i(8)
+  u_page_inserter.request_i <> u_bus.slave_requests_o(8)
+
+  // The page buffer
+  val u_page_buffer = Module(new peripheral.PageBuffer)
+  u_page_buffer.S_AXI // From QEMU (RW)
+  u_page_buffer.normal_read_reply_o <> u_page_inserter.read_reply_i
+  u_page_buffer.normal_read_request_i <> u_page_inserter.read_request_o
+  u_page_buffer.normal_write_request_i <> u_page_deletor.page_buffer_write_o
+
+  // QEMU Message complex
+  val u_qemu_message_compositor = Module(new QEMUMessageCompositor())
+  u_qemu_message_compositor.evict_done_req_i <> u_page_deletor.done_message_o
+  u_qemu_message_compositor.evict_notify_req_i <> u_page_deletor.start_message_o
+  u_qemu_message_compositor.page_fault_req_i <> u_miss_to_qemu.queue_o 
+
+  val u_qemu_message_sender = Module(new peripheral.QEMUMessageSender())
+  u_qemu_message_sender.M_AXI // To QEMU (W)
+  u_qemu_message_sender.fifo_i <> u_qemu_message_compositor.o
+
+  // Demander Message complex
+  val u_demander_message_complex = Module(new DemanderMessageCompositor())
+  
+  u_demander_message_complex.o <> u_r_mmq.queue_i
+
+  val u_itlb_message_convert = Module(new peripheral.TLBMessageConverter(param.toTLBParameter()))
+  u_itlb_message_convert.eviction_request_o <> u_demander_message_complex.itlb_evict_request_i
+  u_itlb_message_convert.miss_request_o <> u_demander_message_complex.itlb_miss_request_i
+  u_itlb_message_convert.tlb_backend_request_i
+
+  val u_dtlb_message_convert = Module(new peripheral.TLBMessageConverter(param.toTLBParameter()))
+  u_dtlb_message_convert.eviction_request_o := u_demander_message_complex.dtlb_evict_request_i
+  u_dtlb_message_convert.miss_request_o := u_demander_message_complex.dtlb_miss_request_i
+  u_dtlb_message_convert.tlb_backend_request_i
+
+  val u_qemu_to_demander_message_conveter = Module(new peripheral.QEMUMessageConverter())
+  u_demander_message_complex.qemu_miss_reply_i <> u_qemu_to_demander_message_conveter.miss_reply_o 
+  u_demander_message_complex.qemu_evict_reply_i <> u_qemu_to_demander_message_conveter.evict_reply_o
+
+  val u_qemu_message_receiver = Module(new peripheral.QEMUMessageReceiver({
+    addr => true.B // TODO: Replace with correct function.
+  }))
+  u_qemu_message_receiver.S_AXI // From QEMU (R)
+  u_qemu_message_receiver.fifo_o <> u_qemu_to_demander_message_conveter.i
+
 
   // TODO: DRAM Reset
 
--- src/main/scala/demander/peripheral/PCIEAdaptor.scala
+++ src/main/scala/demander/peripheral/PCIEAdaptor.scala
@@ -6,17 +6,22 @@ import chisel3.util._
 import DMAController.Bus._
 import armflex.util.AXIRAMController
 import armflex.demander.software_bundle.QEMUMessage
+import armflex.demander.software_bundle.PageDemanderMessage
+import armflex.demander.software_bundle.QEMUEvictReply
+import armflex.demander.software_bundle.QEMUMissReply
+import armflex.demander.software_bundle.PageDemanderMessageType
 
 class QEMUMessageReceiver (
   trigger: (UInt) => Bool
 ) extends MultiIOModule {
   val S_AXI = IO(Flipped(new AXI4(64, 512)))
-  val fifo_o = IO(Decoupled(UInt(512.W)))
+  val fifo_o = IO(Decoupled(new PageDemanderMessage))
   val u_axi_ram_controller = Module(new AXIRAMController(64, 512))
 
   u_axi_ram_controller.S_AXI <> S_AXI
   u_axi_ram_controller.read_reply_i := DontCare
-  fifo_o.bits := u_axi_ram_controller.write_request_o.bits.data
+  fifo_o.bits := fifo_o.bits.parseFromVec(VecInit(u_axi_ram_controller.write_request_o.bits.data.asBools().grouped(32).map(Cat(_)).toSeq))
+
   fifo_o.valid := u_axi_ram_controller.write_request_o.valid && trigger(u_axi_ram_controller.write_request_o.bits.addr)
   u_axi_ram_controller.write_request_o.ready := fifo_o.ready
 }
@@ -42,3 +47,24 @@ class QEMUMessageSender (
   fifo_q.ready := u_dma_write.io.xfer.done
 }
 
+class QEMUMessageConverter(
+  fifoDepth: Int = 2
+) extends MultiIOModule {
+  val i = IO(Flipped(Decoupled(new PageDemanderMessage)))
+
+  val miss_reply_o = IO(Decoupled(new QEMUMissReply))
+  val evict_reply_o = IO(Decoupled(new QEMUEvictReply))
+
+  val miss_reply_enq = Wire(Decoupled(new QEMUMissReply))
+  miss_reply_enq.valid := i.valid && i.bits.message_type === PageDemanderMessageType.sQEMUMissReply
+  miss_reply_enq.bits := miss_reply_enq.bits.parseFromVec(i.bits.data)
+
+  val evict_reply_enq = Wire(Decoupled(new QEMUEvictReply))
+  evict_reply_enq.valid := i.valid && i.bits.message_type === PageDemanderMessageType.sQEMUEvictReply
+  evict_reply_enq.bits := evict_reply_enq.bits.parseFromVec(i.bits.data)
+
+  miss_reply_o <> Queue(miss_reply_enq, fifoDepth)
+  evict_reply_enq <> Queue(evict_reply_enq, fifoDepth)
+
+  i.ready := miss_reply_enq.fire() || evict_reply_enq.fire()
+}
