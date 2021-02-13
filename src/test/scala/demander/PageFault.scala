caa114837c355aef2a5e04d0dd6832ef3d53d9a4
Shanqing Lin <xusine@outlook.com>
2021-02-13 18:06:39 +0100

    Pass old tests!

--- src/test/scala/demander/PageFault.scala
+++ src/test/scala/demander/PageFault.scala
@@ -19,7 +19,7 @@ import armflex.cache.MemorySystemParameter
 
 class PageDemanderPageFaultTester extends FreeSpec with ChiselScalatestTester {
   import PageDemanderDriver._
-  "Normal" ignore {
+  "Normal" in {
     val anno = Seq(TargetDirAnnotation("test/demander/pagefault/normal"), VerilatorBackendAnnotation, WriteVcdAnnotation)
     test(new PageDemanderDUT(new MemorySystemParameter(), "src/main/cpp/demander.txt")).withAnnotations(anno){ dut =>
       // apply a miss request
@@ -35,14 +35,14 @@ class PageDemanderPageFaultTester extends FreeSpec with ChiselScalatestTester {
         dut.itlb_backend_request_i.ready.expect(true.B)
         dut.tk()
       }
-      dut.waitForSignalToBe(dut.M_AXI_PTSet.ar.arvalid)
+      dut.waitForSignalToBe(dut.M_AXI_PW.ar.arvalid)
       // Ok, how to handle the reply?
       // Maybe we should add a DRAM here? Although I can manually reply to it. (Let's do it manually.)
-      dut.M_AXI_PTSet.ar.araddr.expect((0xAB * 3 * 64).U)
-      dut.M_AXI_PTSet.ar.arlen.expect(2.U)
+      dut.M_AXI_PW.ar.araddr.expect((0xAB * 3 * 64).U)
+      dut.M_AXI_PW.ar.arlen.expect(2.U)
       // accept the read request
       timescope {
-        dut.M_AXI_PTSet.ar.arready.poke(true.B)
+        dut.M_AXI_PW.ar.arready.poke(true.B)
         dut.tk()
       }
       // prepare for the data
@@ -56,22 +56,22 @@ class PageDemanderPageFaultTester extends FreeSpec with ChiselScalatestTester {
       // pushing data to the dut.
       timescope {
         for(i <- 0 until 3){
-          dut.M_AXI_PTSet.r.rdata.poke(dut.pageset_converter_raw_o(i).peek)
-          dut.M_AXI_PTSet.r.rid.poke(0.U)
-          dut.M_AXI_PTSet.r.rlast.poke((i == 2).B)
-          dut.M_AXI_PTSet.r.rresp.poke(0.U)
-          dut.M_AXI_PTSet.r.rvalid.poke(true.B)
-          dut.M_AXI_PTSet.r.rready.expect(true.B)
+          dut.M_AXI_PW.r.rdata.poke(dut.pageset_converter_raw_o(i).peek)
+          dut.M_AXI_PW.r.rid.poke(0.U)
+          dut.M_AXI_PW.r.rlast.poke((i == 2).B)
+          dut.M_AXI_PW.r.rresp.poke(0.U)
+          dut.M_AXI_PW.r.rvalid.poke(true.B)
+          dut.M_AXI_PW.r.rready.expect(true.B)
           dut.tk()
         }
       }
       // wait for the reply to the TLB
-      dut.waitForSignalToBe(dut.tlb_backend_reply_o(0).valid)
-      dut.tlb_backend_reply_o(0).bits.tag.thread_id.expect(0.U)
-      dut.tlb_backend_reply_o(0).bits.tag.vpage.expect(0xABC.U)
-      dut.tlb_backend_reply_o(0).bits.data.modified.expect(false.B)
-      dut.tlb_backend_reply_o(0).bits.data.pp.expect(0xCBA.U)
-      dut.tlb_backend_reply_o(0).bits.data.permission.expect(2.U)
+      dut.waitForSignalToBe(dut.itlb_backend_reply_o.valid)
+      dut.itlb_backend_reply_o.bits.tag.thread_id.expect(0.U)
+      dut.itlb_backend_reply_o.bits.tag.vpage.expect(0xABC.U)
+      dut.itlb_backend_reply_o.bits.data.modified.expect(false.B)
+      dut.itlb_backend_reply_o.bits.data.pp.expect(0xCBA.U)
+      dut.itlb_backend_reply_o.bits.data.permission.expect(2.U)
     }
   }
 
@@ -91,59 +91,59 @@ class PageDemanderPageFaultTester extends FreeSpec with ChiselScalatestTester {
         dut.dtlb_backend_request_i.ready.expect(true.B)
         dut.tk()
       }
-      dut.waitForSignalToBe(dut.M_AXI_PTSet.ar.arvalid)
+      dut.waitForSignalToBe(dut.M_AXI_PW.ar.arvalid)
       // Ok, how to handle the reply?
       // Maybe we should add a DRAM here? Although I can manually reply to it. (Let's do it manually.)
-      dut.M_AXI_PTSet.ar.araddr.expect((0xAB * 3 * 64).U)
-      dut.M_AXI_PTSet.ar.arlen.expect(2.U)
+      dut.M_AXI_PW.ar.araddr.expect((0xAB * 3 * 64).U)
+      dut.M_AXI_PW.ar.arlen.expect(2.U)
       // accept the read request
       timescope {
-        dut.M_AXI_PTSet.ar.arready.poke(true.B)
+        dut.M_AXI_PW.ar.arready.poke(true.B)
         dut.tk()
       }
       // pushing data to the dut.
       timescope {
         for(i <- 0 until 3){
-          dut.M_AXI_PTSet.r.rdata.poke(dut.pageset_converter_raw_o(i).peek)
-          dut.M_AXI_PTSet.r.rid.poke(0.U)
-          dut.M_AXI_PTSet.r.rlast.poke((i == 2).B)
-          dut.M_AXI_PTSet.r.rresp.poke(0.U)
-          dut.M_AXI_PTSet.r.rvalid.poke(true.B)
-          dut.M_AXI_PTSet.r.rready.expect(true.B)
+          dut.M_AXI_PW.r.rdata.poke(dut.pageset_converter_raw_o(i).peek)
+          dut.M_AXI_PW.r.rid.poke(0.U)
+          dut.M_AXI_PW.r.rlast.poke((i == 2).B)
+          dut.M_AXI_PW.r.rresp.poke(0.U)
+          dut.M_AXI_PW.r.rvalid.poke(true.B)
+          dut.M_AXI_PW.r.rready.expect(true.B)
           dut.tk()
         }
       }
       // This will trigger an page fault.
       println("Page fault should have been triggered. Check it now.")
-      dut.waitForSignalToBe(dut.M_AXI_QEMU_Message.aw.awvalid)
-      dut.M_AXI_QEMU_Message.aw.awaddr.expect(0.U)
+      dut.waitForSignalToBe(dut.M_AXI_QEMUTX.aw.awvalid)
+      dut.M_AXI_QEMUTX.aw.awaddr.expect(0.U)
       // ack it
       timescope {
-        dut.M_AXI_QEMU_Message.aw.awready.poke(true.B)
+        dut.M_AXI_QEMUTX.aw.awready.poke(true.B)
         dut.tk()
       }
       // wait for the data
       println("Wait for the data part to be there")
-      dut.waitForSignalToBe(dut.M_AXI_QEMU_Message.w.wvalid)
+      dut.waitForSignalToBe(dut.M_AXI_QEMUTX.w.wvalid)
       // What's the data?
       // 0: valid 1: message_type(4.U) 2: tag.vpn(31:0), 3: tag.vpn(51, 32), 4: process_id, 5: permission
       val data_seq = Seq[BigInt](1, 4, 0xABC, 0, 10, 0)
-      dut.M_AXI_QEMU_Message.w.wdata.expect(
+      dut.M_AXI_QEMUTX.w.wdata.expect(
         data_seq.reverse.foldLeft(BigInt(0))({ case(res, cur) =>
           (res << 32) | cur
         }).U
       )
-      dut.M_AXI_QEMU_Message.w.wlast.expect(true.B)
+      dut.M_AXI_QEMUTX.w.wlast.expect(true.B)
       // Mark the writeback accepted.
       timescope {
-        dut.M_AXI_QEMU_Message.w.wready.poke(true.B)
+        dut.M_AXI_QEMUTX.w.wready.poke(true.B)
         dut.tk()
       }
       // write response
       timescope {
-        dut.M_AXI_QEMU_Message.b.bvalid.poke(true.B)
-        dut.M_AXI_QEMU_Message.b.bresp.poke(0.U)
-        dut.M_AXI_QEMU_Message.b.bready.expect(true.B)
+        dut.M_AXI_QEMUTX.b.bvalid.poke(true.B)
+        dut.M_AXI_QEMUTX.b.bresp.poke(0.U)
+        dut.M_AXI_QEMUTX.b.bready.expect(true.B)
         dut.tk()
       }
     }
