491f4be19c378e8448aba1fc9d543323aee2ada5
Cyan Lin <shanqing.lin@epfl.ch>
2022-01-14 11:30:42 +0100

    Github Action Should Run Unit Tests Defined in CPP (#31)
    * Remove unnecessary code
    
    * Github Action: It runs the cpp unit test now
    
    * Add dependency checker
    
    * GHA: Remove sudo + Specify make type
    
    * Well, sudo should be back
    
    * Enforce the working directory

---
+++ regression/runAll.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 1. make sure two test file exists
+
+if [ ! -f build/KnockoutSimulator ] || [ ! -f build/KnockoutTestGenerator ]; then
+  echo "Please build the simulator first."
+  exit -1
+fi
+
+# 1.1 switch into build folder
+
+cd build
+
+# 2. Generate test list
+
+allTests=("multiple-pages-in-a-row" "transplant-in" "MMU-push-and-evict-pte" "basic-transplant-with-initial-page-fault" "transplant-transplants" "execute-instruction-with-context-in-dram" "execute-instruction")
+
+# 3. Run each
+
+for t in ${allTests[@]}; do
+  # 3.1 start the server
+  ./KnockoutSimulator &
+  sleep 1
+  # 3.2 start the client
+  ./KnockoutTestGenerator $t
+  if [ $? != 0 ]; then
+    echo "Run test $t failed."
+    exit 127
+    cd ..
+  fi
+  sleep 1
+done
+
+cd ..
