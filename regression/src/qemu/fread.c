b44b359aeaeeb66b5b18f717746039cb0203103e
Rafael Ulises Luzius Pizarro Solar <rafael.pizarrosolar@epfl.ch>
2022-03-18 11:46:06 +0100

    SVC tests

---
+++ regression/src/qemu/fread.c
@@ -0,0 +1,32 @@
+#define STR(x)  #x
+#define XSTR(s) STR(s)
+#define magic_inst(val) __asm__ __volatile__ ( "hint " XSTR(val) " \n\t"  )
+
+#define DO_QFLEX_OP(op) magic_inst(QFLEX_OP); magic_inst(op)
+
+#define DEVTEROFLEX_OP    (94)
+
+#define DEVTEROFLEX_FLOW_START (90)
+#define DEVTEROFLEX_FLOW_STOP  (91)
+#define DO_DEVTEROFLEX_OP(op) magic_inst(DEVTEROFLEX_OP); magic_inst(op)
+
+#include <stdio.h>
+#include <assert.h>
+
+int main(){
+	FILE *f = fopen(__FILE__, "r");
+
+	DO_DEVTEROFLEX_OP(DEVTEROFLEX_FLOW_START);
+
+	char x = getc(f);
+
+	DO_DEVTEROFLEX_OP(DEVTEROFLEX_FLOW_STOP);
+
+	assert(x == '#' && "Expect to get the char #.");
+
+	puts("Success!");
+
+	fclose(f);
+	return 0;
+}
+
