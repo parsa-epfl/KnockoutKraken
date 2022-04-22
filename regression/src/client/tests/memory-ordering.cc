a79518f1bfbc36ff8ae08f20ca42511425e3752e
xusine <xusine@outlook.com>
2022-04-22 13:48:37 +0200

    A stronger test for memory ordering.

--- regression/src/client/tests/memory-ordering.cc
+++ regression/src/client/tests/memory-ordering.cc
@@ -10,10 +10,12 @@ TEST_CASE("transplant-data-and-request-order") {
   DevteroflexArchState state;
   DevteroflexArchState stateOut;
   initFPGAContextAndPage(1, &c);
-  initArchState(&state, rand());
+  initArchState(&state, 0xC000);
+  DevteroflexArchState differentState;
+  initArchState(&differentState, 0x4000);
   MessageFPGA msg;
 
-  mmuRegisterTHID2ASID(&c, 0, 0x10);
+  mmuRegisterTHID2ASID(&c, 7, 0x10);
   transplantPushState(&c, 0, &state);
   transplantPushState(&c, 1, &state);
   transplantPushState(&c, 2, &state);
@@ -21,22 +23,22 @@ TEST_CASE("transplant-data-and-request-order") {
   transplantPushState(&c, 4, &state);
   transplantPushState(&c, 5, &state);
   transplantPushState(&c, 6, &state);
-  transplantPushState(&c, 7, &state);
-  transplantStart(&c, 3);
+  transplantPushState(&c, 7, &differentState);
+  transplantStart(&c, 7);
 
   mmuMsgGet(&c, &msg);
 
-  REQUIRE(msg.vpn == state.pc);
+  REQUIRE(msg.vpn == (differentState.pc >> 12));
   REQUIRE(msg.asid == 0x10);
 
-  transplantForceTransplant(&c, 3);
+  transplantForceTransplant(&c, 7);
   
   uint32_t pending;
   transplantWaitTillPending(&c, &pending);
 
-  REQUIRE(pending & 1 << 3);
-  transplantGetState(&c, 3, &stateOut);
-  REQUIRE(state.pc == stateOut.pc);
+  REQUIRE(pending & 1 << 7);
+  transplantGetState(&c, 7, &stateOut);
+  REQUIRE(differentState.pc == stateOut.pc);
 
   releaseFPGAContext(&c);
 }
